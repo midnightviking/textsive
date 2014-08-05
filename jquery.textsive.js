3b410379768820712bc6c1217cd7a1fd01616821
midnightviking <mike@midnightviking.com>
2014-08-04 23:04:57 -0400

    Initial Uploads

---
+++ jquery.textsive.js
@@ -0,0 +1,51 @@
+// Textsive
+(function($){
+  //Text + Responsive. Its not that clever huh?
+  $.fn.textsive = function(options){
+    //Accept Options
+    var settings = $.extend({
+      minFontSize:10,
+      maxFontSize:1000
+    },options);
+    
+    //Return all instances
+    return this.each(function(){
+      var $this = $(this);
+      //Regex finding all the <BR> <br> <br/> \r \n to determine line-breaks
+      var newLines = $this.html().trim().split(/[\r\n]|<br\s*[\/]?>/gi);
+      var nLines = [];
+      //Clear out the original bit of text. I might need to make a less destructive method
+      $this.text("");
+      
+      //Now that the text has been broken into its lines we need to wrap it in a block object to later detect the width. 
+      for(var i = 0; i < newLines.length; i++){
+        nLines[i] = ("<div>"+newLines[i]+"</div>");        
+      }
+      //Throw it back in now that "new lines" have been broken into full blocks
+      $this.append(nLines);
+      
+      //So heres the function that gets called whenever the window is resized.
+      //Already setup the "line-breaks" so we don't have to reconstruct those
+      var resizeMe = function(){
+          $this.css({
+            color    : settings.color
+          });
+        
+        /*So the thought here is to take the width of the container divided
+        by the number of characters. Instead of the text being uniformly the
+        the same size  I can get that nice scale variance where the characters
+        form a block. Clamping function for some control, but I like it without.
+        */
+        $this.find("div").each(function(i){
+          var c = $(this).width() / $(this).text().trim().length;
+          $(this).css({
+              fontSize:  Math.min(Math.max(c, settings.minFontSize), settings.maxFontSize) 
+          });
+        });
+      };
+      
+      resizeMe();
+      $(window).on("resize",resizeMe);
+    });
+  };  
+}(jQuery));
